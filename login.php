3bc39a6251b51947d918eed3cfb62856b4392919
Donny Rizal Adhi Pratama <44853905+L200183161@users.noreply.github.com>
2020-12-28 23:22:17 +0700

    Login verify berhasil dengan PDO

--- login.php
+++ login.php
@@ -1,27 +1,27 @@
 ﻿<?php
-	session_start();
-	global $wrongpassword,$wrongusername;
-	require_once 'includes/config.php';
-	$dbh = Database();
-	if(isset($_POST['login'])){
-		$_SESSION['userlogin'] = $_POST['username'];
-		$username = htmlspecialchars($_POST['username']);
-		$password = htmlspecialchars($_POST['password']);
-		$sql = "SELECT UserName,Password from users where UserName=:username";
-		$query = $dbh->prepare($sql);
-		$query->bindParam(':username',$username,PDO::PARAM_STR);
-		$query-> execute();
-		$results=$query->fetchAll(PDO::FETCH_OBJ);
-		if($query->rowCount() > 0){
-			foreach ($results as $row) {
-				$hashpass=$row->Password;
-			}//verifying Password
-			if (password_verify($password, $hashpass)) {
-				$_SESSION['userlogin']=$_POST['username'];
+session_start();
+global $wrongpassword, $wrongusername;
+require_once 'includes/config.php';
+$dbh = Database();
+if (isset($_POST['login'])) {
+
+	$username = htmlspecialchars($_POST['username']);
+	$password = htmlspecialchars($_POST['password']);
+	$sql = "SELECT username, password from users where username = '$username'";
+	// $query = $dbh->prepare($sql);
+	// $query->bindParam(':username',$username,PDO::PARAM_STR);
+	// $query-> execute();
+	// $results = $query->fetchAll(PDO::FETCH_OBJ);
+	$results = $dbh->query($sql);
+	if ($results->rowCount() > 0) {
+		foreach ($results as $row) {
+			$_SESSION['role'] = $row['role'];
+			$_SESSION['userlogin'] = $_POST['username'];
+			if (password_verify($password, $row['password'])) {
+				$_SESSION['userlogin'] = $_POST['username'];
 				echo "<script>window.location.href= 'index.php'; </script>";
-			}
-			else {
-				$wrongpassword='
+			} else {
+				$wrongpassword = '
 				<div class="alert alert-danger alert-dismissible fade show" role="alert">
 				<strong>Oh Snapp!😕</strong> Alert <b class="alert-link">Password: </b>You entered wrong password.
 				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -29,21 +29,24 @@
 				</button>
 				</div>';
 			}
-		}
-		//if username or email not found in database
-		else{
-			$wrongusername='
+		} //verifying Password
+
+	}
+	//if username or email not found in database
+	else {
+		$wrongusername = '
 			<div class="alert alert-danger alert-dismissible fade show" role="alert">
 				<strong>Oh Snapp!🙃</strong> Alert <b class="alert-link">UserName: </b> You entered a wrong UserName.
 				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
 				</button>
 			</div>';
-		}
 	}
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
@@ -84,40 +87,44 @@
 						<p class="account-subtitle">Access to our dashboard</p>
 						<!-- Account Form -->
 						<form method="POST" enctype="multipart/form-data">
-								<div class="form-group">
-									<label>User Name</label>
-									<input class="form-control" name="username" required type="text">
-								</div>
-								<?php if($wrongusername){echo $wrongusername;}?>
-								<div class="form-group">
-									<div class="row">
-										<div class="col">
-											<label>Password</label>
-										</div>
-									</div>
-									<input class="form-control" name="password" required type="password">
-								</div>
-								<?php if($wrongpassword){echo $wrongpassword;}?>
-								<div class="form-group">
-									<div class="form-check">
-										<input id="remember_me" class="form-check-input" type="checkbox">
-											<label class="form-check-label" for="remember_me">Remember Me
-											</label>
+							<div class="form-group">
+								<label>User Name</label>
+								<input class="form-control" name="username" required type="text">
+							</div>
+							<?php if ($wrongusername) {
+								echo $wrongusername;
+							} ?>
+							<div class="form-group">
+								<div class="row">
+									<div class="col">
+										<label>Password</label>
 									</div>
 								</div>
-								<div class="form-group text-center">
-									<button class="btn btn-primary account-btn" name="login" type="submit">Login</button>
-										<div class="col-auto pt-2">
-											<a class="text-muted float-right" href="forgot-password.php">
-												Forgot password?
-											</a>
-										</div>
+								<input class="form-control" name="password" required type="password">
+							</div>
+							<?php if ($wrongpassword) {
+								echo $wrongpassword;
+							} ?>
+							<div class="form-group">
+								<div class="form-check">
+									<input id="remember_me" class="form-check-input" type="checkbox">
+									<label class="form-check-label" for="remember_me">Remember Me
+									</label>
 								</div>
-									
-								<div class="account-footer">
-									<p>Having Trouble? report an issue on github <a target="https://github.com/MusheAbdulHakim/smarthr/issues" href="https://github.com/MusheAbdulHakim/smarthr/issues">Github issues</a></p>
+							</div>
+							<div class="form-group text-center">
+								<button class="btn btn-primary account-btn" name="login" type="submit">Login</button>
+								<div class="col-auto pt-2">
+									<a class="text-muted float-right" href="forgot-password.php">
+										Forgot password?
+									</a>
 								</div>
-							</form>
+							</div>
+
+							<div class="account-footer">
+								<p>Having Trouble? report an issue on github <a target="https://github.com/MusheAbdulHakim/smarthr/issues" href="https://github.com/MusheAbdulHakim/smarthr/issues">Github issues</a></p>
+							</div>
+						</form>
 						<!-- /Account Form -->
 					</div>
 				</div>
@@ -133,4 +140,5 @@
 	<!-- Custom JS -->
 	<script src="assets/js/app.js"></script>
 </body>
+
 </html>
